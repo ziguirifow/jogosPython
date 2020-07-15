e0ec341a3672d4f6806ad5ea6a501cb7481f278d
Thulio <thulio.alves@gmail.com>
2020-07-15 03:18:29 -0300

    Substituindo o loop do jogo de while para o for

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,9 +4,8 @@ print("*********************************")
 
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
 
-while rodada <= total_de_tentativas:
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute = int(input("Digite o seu numero: "))
     print("Você digitou", chute)
@@ -22,7 +21,5 @@ while rodada <= total_de_tentativas:
             print("Você errou! O seu chute foi maior que o número secreto.")
         elif menor:
             print("Você errou! O seu chute foi menor que o número secreto.")
-
-    rodada = rodada + 1
 
 print("Fim do jogo!")
