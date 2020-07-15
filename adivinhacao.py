8435974680b87fe1e71c000651a29beca1812b39
Thulio <thulio.alves@gmail.com>
2020-07-14 23:27:23 -0300

    Arrumando variáveis, deixando mais legível o código, adicionando condicional para verificar seu o número é mair ou menor

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,13 +4,20 @@ print("*********************************")
 
 numero_secreto = 42
 
-chute_str = int(input("Digite o seu numero: "))
+chute = int(input("Digite o seu numero: "))
 
-print("Você digitou", chute_str)
+print("Você digitou", chute)
 
-if numero_secreto == chute_str:
+acertou = chute == numero_secreto
+maior   = chute > numero_secreto
+menor   = chute < numero_secreto
+
+if acertou:
     print("Você acertou!")
 else:
-    print("Você errou :(")
+    if maior:
+        print("Você errou! O seu chute foi maior que o número secreto.")
+    elif menor:
+        print("Você errou! O seu chute foi menor que o número secreto.")
 
 print("Fim do jogo!")
