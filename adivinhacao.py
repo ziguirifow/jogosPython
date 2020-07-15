da2ba1c0cc90d29e250fa0344ff9bf222b937c1a
Thulio <thulio.alves@gmail.com>
2020-07-15 03:09:44 -0300

    Implementando o while para o loop do jogo, adicionando .format no print

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,21 +3,26 @@ print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute = int(input("Digite o seu numero: "))
+while rodada <= total_de_tentativas:
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute = int(input("Digite o seu numero: "))
+    print("Você digitou", chute)
 
-print("Você digitou", chute)
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
+    if acertou:
+        print("Você acertou!")
+    else:
+        if maior:
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif menor:
+            print("Você errou! O seu chute foi menor que o número secreto.")
 
-if acertou:
-    print("Você acertou!")
-else:
-    if maior:
-        print("Você errou! O seu chute foi maior que o número secreto.")
-    elif menor:
-        print("Você errou! O seu chute foi menor que o número secreto.")
+    rodada = rodada + 1
 
 print("Fim do jogo!")
