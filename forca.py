29e1a6cb938ce931b961bbc820bbc5f8053caebf
Thulio <thulio.alves@gmail.com>
2020-07-19 18:21:50 -0300

    Adicionando a função strip e diferenciando o chute de maiusculo para minusculo

--- forca.py
+++ forca.py
@@ -11,10 +11,11 @@ def jogar():
     while not enforcou and not acertou:
 
         chute = input("Qual letra?")
+        chute = chute.strip()
 
         index = 0
         for letra in palavra_secreta:
-            if chute == letra:
+            if chute.upper() == letra.upper():
                 print("Encontrei a letra {} na posição {}".format(letra, index))
             index = index + 1
 
